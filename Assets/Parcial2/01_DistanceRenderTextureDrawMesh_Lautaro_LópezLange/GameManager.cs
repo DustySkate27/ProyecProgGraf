7ac272fd3ab52b2cf5fbf72d739b30f9c2161aec
Lautaro López Lange <lautarolopezutn@gmail.com>
2026-06-23 18:57:10 -0300

    partial push

--- Assets/Parcial2/01_DistanceRenderTextureDrawMesh_Lautaro_LópezLange/GameManager.cs
+++ Assets/Parcial2/01_DistanceRenderTextureDrawMesh_Lautaro_LópezLange/GameManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance;
+
+    [SerializeField] private List<GameObject> assetList;
+    [SerializeField] private GameObject cam;
+    private GameObject currentGoal;
+    [SerializeField] private LayerMask objLayer;
+    private int points;
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+            Instance = this;
+        }
+        else
+            DontDestroyOnLoad(this);
+
+            points = 0;
+        InitCam();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InitCam()
     {
-        
+        currentGoal = assetList[Random.Range(0, assetList.Count)];
+        cam.transform.position = new Vector3 (currentGoal.transform.position.x, cam.transform.position.y, currentGoal.transform.position.z);
     }
+
+    private void HitsTheTarget(Transform raycaster)
+    {
+        if (Physics.Raycast(raycaster.position, raycaster.forward, out RaycastHit hit, 100f) && hit.transform == currentGoal.transform)
+        {
+            currentGoal = null;
+            points++;
+            InitCam();
+        }
+    }
+
 }
